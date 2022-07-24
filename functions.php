271bfcab35dee391996deb083d08d210a9a07ed7
rink <77056883+rin-k-dev@users.noreply.github.com>
2022-07-25 02:01:15 +0900

    update functions.php

--- functions.php
+++ functions.php
@@ -1,43 +1,46 @@
 <?php
-function portfolio_setup() {
+if (!function_exists('portfolio_setup')) {
+    function portfolio_setup()
+    {
 
-    add_theme_support( 'wp-block-styles' );
+        add_theme_support('wp-block-styles');
 
-    add_theme_support( 'responsive-embeds' );
+        add_theme_support('responsive-embeds');
 
-    add_theme_support( 'editor-styles' );
-    add_editor_style( 'editor-style.css' );
+        add_theme_support('editor-styles');
+        add_editor_style('editor-style.css');
 
-    add_theme_support( 'title-tag' );
+        add_theme_support('title-tag');
 
-    add_theme_support( 'html5', array( 
-		'style', 
-		'script' 
-	) );
+        add_theme_support('html5', array(
+            'style',
+            'script'
+        ));
 
-    add_theme_support( 'post-thumbnails' );
+        add_theme_support('post-thumbnails');
 
-    add_theme_support( 'align-wide' );
+        add_theme_support('align-wide');
 
-    register_nav_menus( array(
-		'primary' => 'ナビゲーション'
-	) );
-
+        register_nav_menus(array(
+            'primary' => 'ナビゲーション'
+        ));
+    }
 }
-add_action( 'after_setup_theme', 'portfolio_setup' );
+add_action('after_setup_theme', 'portfolio_setup');
 
-function portfolio_scripts() {
+function portfolio_scripts()
+{
 
-    wp_enqueue_style( 
-		'dashicons' 
-	);
+    wp_enqueue_style(
+        'dashicons'
+    );
 
-    wp_enqueue_script( 
+    wp_enqueue_script(
         'portfolio-adobe-fonts',
         get_template_directory_uri() . '/src/js/adobe.js',
         array(),
-        filemtime( get_theme_file_path() . '/src/js/adobe.js' ),
-        true
+        filemtime(get_theme_file_path('/src/js/adobe.js')),
+        false
     );
 }
-add_action( 'wp_enqueue_scripts',  'portfolio_scripts');
+add_action('wp_enqueue_scripts',  'portfolio_scripts');
