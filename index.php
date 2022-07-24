c59e2515e6223e5807775c68b6804da0d2f5d238
rink <77056883+rin-k-dev@users.noreply.github.com>
2022-07-24 19:29:03 +0900

    update index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>    
+
+<?php  
+    $args = array(
+        'category_name' => 'works',
+        'order' => 'ASC'
+    );
+    $the_query = new WP_Query( $args );
+?>
+
+<?php if($the_query->have_posts()): while($the_query->have_posts()):
+    $the_query->the_post(); ?>
+    <?php the_post_thumbnail(); ?>
+    <?php the_title(); ?>
+    <?php wp_reset_postdata(); ?>
+<?php endwhile; ?>
+<?php else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>    
+<?php wp_footer(); ?>
+</body>
+</html>
