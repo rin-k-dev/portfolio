a20f9d1680fdbe1e2a5b3135a3d3736e92a8ee63
rink <77056883+rin-k-dev@users.noreply.github.com>
2022-08-02 01:32:39 +0900

    Update pages.php app.css

--- page.php
+++ page.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <main role="main" class="main">
-            <?php the_content(); ?>
-    <?php endwhile;
+                <main role="main" class="main">
+                        <?php the_content(); ?>
+        <?php endwhile;
 endif; ?>
-        </main>
+                </main>
 
-        <?php get_footer(); ?>
+                <?php get_footer(); ?>
