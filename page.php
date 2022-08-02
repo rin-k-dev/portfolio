def5bd819cd8c29e7a63e416d5e1bcb5a408e770
rink <77056883+rin-k-dev@users.noreply.github.com>
2022-08-02 12:45:43 +0900

    Update page.php

--- page.php
+++ page.php
@@ -1,9 +1,8 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <main role="main" class="main">
-                        <?php the_content(); ?>
-        <?php endwhile;
+    <main role="main" class="main">
+      <?php the_content(); ?>
+  <?php endwhile;
 endif; ?>
-                </main>
-
-                <?php get_footer(); ?>
+    </main>
+    <?php get_footer(); ?>
